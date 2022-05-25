8005ed3a15b81b04fb8dd0cf0e50a8704071698b
Rui Soares <up202103631@up.pt>
2022-05-25 17:34:37 +0100

    Atualização Graph

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -6,3 +6,16 @@
 
 Graph::Graph(int num) : n(num), nodes(num+1) {
 }
+
+void Graph::addEdge(int src, int dest, double cap, double dur) {
+    nodes[src].adj.push_back({dest, cap, 0, dur, 0});
+}
+
+void Graph::updateEdge(int src, int dest, double addCapacity, double addDuration){
+    for(auto e: nodes[src].adj){
+        if(e.dest == dest){
+            e.crtCap += addCapacity;
+            e.crtDur += addDuration;
+        }
+    }
+}
