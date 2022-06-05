3fb763ffefd95378ed084e4cac42443596f79b0e
david <davidespinhamarques@gmail.com>
2022-06-05 04:26:25 +0100

    fixed 2-5

--- main.cpp
+++ main.cpp
@@ -1,12 +1,7 @@
 #include "includes/Menu.h"
 
 int main(){
-//    Menu m;
-//    m.run();
-    FileReader f;
-    Graph g = f.readFile("in01_b.txt");
-
-    std::cout <<g.calcPathTime({3,9,27,44}) << " ";
-    std::cout <<g.calcPathTime({3,9,35,31,48,44}) << " ";
+    Menu m;
+    m.run();
     return 0;
 }
